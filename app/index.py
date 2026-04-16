2be022babb44c1eb319b1b79c721ce31c820880a
Byraboyina Bhargavi <132429467+Byraboyina-Bhargavi-03@users.noreply.github.com>
2026-04-16 20:15:26 +0530

    Add home endpoint to FastAPI application

--- app/index.py
+++ app/index.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.orm import Session
-
 from database import Base, engine, SessionLocal
 import crud, schemas
 from worker import process_job
@@ -9,7 +8,10 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency
+@app.get("/")
+def home():
+    return {"message": "FastAPI is running"}
+
 def get_db():
     db = SessionLocal()
     try:
@@ -17,22 +19,15 @@ def get_db():
     finally:
         db.close()
 
-# Create Job
 @app.post("/jobs/", response_model=schemas.JobResponse)
 def create_job(background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     job = crud.create_job(db)
-
-    # Run background task
     background_tasks.add_task(process_job, job.id)
-
     return job
 
-# Get Job Status
 @app.get("/jobs/{job_id}", response_model=schemas.JobResponse)
 def get_job(job_id: int, db: Session = Depends(get_db)):
     job = crud.get_job(db, job_id)
-
     if not job:
         raise HTTPException(status_code=404, detail="Job not found")
-
     return job
