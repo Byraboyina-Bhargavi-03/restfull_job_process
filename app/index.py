53ce41fd4094328f443e25e527781a8fc2a1ebc5
Byraboyina Bhargavi <132429467+Byraboyina-Bhargavi-03@users.noreply.github.com>
2026-04-17 09:42:44 +0530

    Add job creation and status endpoints to FastAPI
    Refactor imports and add job creation and status endpoints.

--- app/index.py
+++ app/index.py
@@ -1,17 +1,14 @@
 from fastapi import FastAPI, Depends, HTTPException, BackgroundTasks
 from sqlalchemy.orm import Session
-from database import Base, engine, SessionLocal
-import crud, schemas
-from worker import process_job
+from .database import Base, engine, SessionLocal
+from . import crud, schemas
+from .worker import process_job
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "FastAPI is running"}
-
+# Dependency
 def get_db():
     db = SessionLocal()
     try:
@@ -19,15 +16,22 @@ def get_db():
     finally:
         db.close()
 
+# Create Job
 @app.post("/jobs/", response_model=schemas.JobResponse)
 def create_job(background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
     job = crud.create_job(db)
+
+    # Run background task
     background_tasks.add_task(process_job, job.id)
+
     return job
 
+# Get Job Status
 @app.get("/jobs/{job_id}", response_model=schemas.JobResponse)
 def get_job(job_id: int, db: Session = Depends(get_db)):
     job = crud.get_job(db, job_id)
+
     if not job:
         raise HTTPException(status_code=404, detail="Job not found")
+
     return job
